132152b6fa74ae2f24baf220c51b9453616f4cde
coconutssss <lu279463768@gmail.com>
2022-06-04 14:04:43 -0400

    JOI Schema Validations

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,9 +4,14 @@ const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate');
 //这个ejs-mate是用于设计layout的
+// const Joi = require('joi');------已经在Schema中require了
+//这个joi是用来在处理validation error 的（在没输入有效内容就鼠标点到其他地方时报错）
+const { campgroundSchema } = require('./schemas.js')
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override');
-const Campground = require('./models/campground')
+const Campground = require('./models/campground');
+const e = require('express');
 
 //在这里connect mongo:
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -29,6 +34,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }))
 // 这个好像是用来保存更新后的元素
 app.use(methodOverride('_method'))
+
+const validateCampground = (req, res, next) => {
+    // //build schema for Joi,this is going to validate our data before we even attempt to save it with Mongoose
+
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+
+}
 
 app.get('/', (req, res) => {
     // res.send('Hello from yelpcamp')
@@ -53,7 +71,16 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
+//create a new campground 用post
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // // if (!req.body.campground.price) {
+
+    // // }
+    // // if (!res.body.campground.image) {
+
+    // // }---------------we are not going to write our own validation,we are going to use a tool that's been made to do just that
+
 
     const campground = new Campground(req.body.campground);
     // res.send(req.body)
@@ -76,7 +103,8 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 //so we can fake a put,patch delete and so on using method override:需要先在bash中下载override:
 //下载：npm i method-override然后需要去require override还需要app.use
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+//update your campground 用put
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     //也可以用findbyId 然后再update thing individually 然后save()
@@ -95,12 +123,19 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }))
 
-//basic error handler:
-app.use((err, req, res, next) => {
-    res.send('Oh boy,somthing went wrong!')
+//this can be only run if nothing else has mathced first and we didn't respond many of them
+app.all('*', (req, res, next) => {
+    // res.send("404!!!!!")
+    next(new ExpressError('Page Not Found', 404))
 })
 
-
+//basic error handler:
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh no,something went wrong!'
+    res.status(statusCode).render('Error', { err })
+    // res.send('Oh boy,somthing went wrong!')
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
