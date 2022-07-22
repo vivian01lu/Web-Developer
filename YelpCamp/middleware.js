6f2cf9dd8a0b91f19236e0827f9fe420517020bc
coconutssss <lu279463768@gmail.com>
2022-07-21 20:53:02 -0400

    refactoring to Campgrounds Controller

--- YelpCamp/middleware.js
+++ YelpCamp/middleware.js
@@ -1,3 +1,10 @@
+const { campgroundSchema, reviewSchema } = require('./schemas.js')
+const ExpressError = require('./utils/ExpressError')
+const Campground = require('./models/campground');
+const Review = require('./models/review');
+
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     // console.log("REQ.USER...", req.user);
     //passport起作用：it is going to be automatically filled in with the deserialized info from the session
@@ -10,4 +17,49 @@ module.exports.isLoggedIn = (req, res, next) => {
         return res.redirect('/login');
     }
     next();
+}
+
+
+
+module.exports.validateCampground = (req, res, next) => {
+    // //build schema for Joi,this is going to validate our data before we even attempt to save it with Mongoose
+
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+
+}
+//create 一个不是当前用户就不能删除campground的middleware:
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next();
+}
+// campgrounds/id/reviews/reviewId
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
 }
--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -30,6 +30,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '62c7881c56a4eac4ddc0020e',
             location: `${cities[random1000].city},${cities[random1000].state}`,
             title: `${sample(descriptors)}${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
